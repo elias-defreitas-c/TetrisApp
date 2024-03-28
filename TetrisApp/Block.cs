4febc194fe9e4c9a42cbd07d1506bffba581b634
elias <defreitase@bzz.ch>
2024-03-28 13:21:45 +0100

    Projektdateien hinzufügen.

---
+++ TetrisApp/Block.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TetrisApp
+{
+    public abstract class Block
+    {
+        protected abstract Position[][] Tiles { get; }
+        protected abstract Position startOffSet { get; }
+        public abstract int ID { get; }
+
+        private int rotationState;
+        private Position offset;
+
+        public Block()
+        {
+            offset = new Position(startOffSet.Row, startOffSet.Column);
+        }
+        public IEnumerable<Position> TilePositions()
+        {
+            foreach (Position p in Tiles[rotationState])
+            {
+                yield return new Position(p.Row + offset.Row, p.Column + offset.Column);
+            }
+        }
+        public void RotateCW()
+        {
+            rotationState = (rotationState + 1) % Tiles.Length;
+        }
+
+        public void RotateCCW()
+        {
+            if (rotationState == 0)
+            {
+                rotationState = Tiles.Length - 1;
+            }
+            else 
+            {
+                rotationState--;
+            }
+        }
+        public void Move(int rows, int  columns)
+        {
+            offset.Row += rows;
+            offset.Column += columns;
+        }
+
+        public void Reset()
+        {
+            rotationState = 0;
+            offset.Row = startOffSet.Row;
+            offset.Column = startOffSet.Column;
+        }
+    }
+}
---
+++ TetrisApp/OBlock.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TetrisApp
+{
+    public class OBlock: Block
+    {
+        private readonly Position[][] tiles = new Position[][]
+        {
+            new Position[] {new(0,0), new(0,1), new(1,0), new(1,1)}
+        };
+
+
+        public override int ID => 4;
+        protected override Position startOffSet => new Position(0,4);
+        protected override Position[][] Tiles => tiles;
+    }
+}
